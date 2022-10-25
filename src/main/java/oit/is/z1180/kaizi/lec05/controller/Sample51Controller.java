69604d5da6f87c8b3613517c9bcd0a3badc3fc95
e1b20087 <e1b20087@oit.ac.jp>
2022-10-25 16:42:55 +0900

    sample5-4,5,6-1まで

--- src/main/java/oit/is/z1180/kaizi/lec05/controller/Sample51Controller.java
+++ src/main/java/oit/is/z1180/kaizi/lec05/controller/Sample51Controller.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -45,35 +45,34 @@ public class Sample51Controller {
     model.addAttribute("fruits2", fruits2);
     return "sample51.html";
   }
-  /*
-   * @GetMapping("step4")
-   *
-   * @Transactional
-   * public String sample54(@RequestParam Integer id, ModelMap model) {
-   * Fruit fruit4 = fMapper.selectById(id);
-   * model.addAttribute("fruit4", fruit4);
-   *
-   * ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
-   * model.addAttribute("fruits2", fruits2);
-   * return "sample51.html";
-   * }
-   *
-   * @PostMapping("step5")
-   * public String sample55(@RequestParam Integer id, @RequestParam String
-   * name, @RequestParam Integer price,
-   * ModelMap model) {
-   * System.out.println("step5");
-   * System.out.println(id);
-   * System.out.println(name);
-   * System.out.println(price);
-   * Fruit fruit = new Fruit(id, name, price);
-   *
-   * fMapper.updateById(fruit);
-   *
-   * ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
-   * model.addAttribute("fruits2", fruits2);
-   *
-   * return "sample51.html";
-   * }
-   */
+
+  @GetMapping("step4")
+
+  @Transactional
+  public String sample54(@RequestParam Integer id, ModelMap model) {
+    Fruit fruit4 = fMapper.selectById(id);
+    model.addAttribute("fruit4", fruit4);
+
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
+  @PostMapping("step5")
+  public String sample55(@RequestParam Integer id, @RequestParam String name, @RequestParam Integer price,
+      ModelMap model) {
+    System.out.println("step5");
+    System.out.println(id);
+    System.out.println(name);
+    System.out.println(price);
+    Fruit fruit = new Fruit(id, name, price);
+
+    fMapper.updateById(fruit);
+
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+
+    return "sample51.html";
+  }
+
 }
