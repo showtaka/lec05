ee537fcdb5cf6b72d51c17eeb24c8051f001c444
showtaka <e1b20060@oit.ac.jp>
2022-10-25 14:05:29 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1180/kaizi/lec05/Lec05Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1180.kaizi.lec05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec05Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1180/kaizi/lec05/Lec05ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1180.kaizi.lec05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
